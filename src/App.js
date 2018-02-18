a96967aa57ebb1b0cc8a0defb423634f7c19f4cc
Evgeniy Podgaetskiy <evgeniypodgaetskiy@gmail.com>
2018-02-18 15:00:14 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,49 +1,95 @@
-import React from "react";
-import Event from "./event";
+import React, { Component } from "react";
 import { events } from "./events";
 
-function EventsList(props) {
+class Event extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      like: false
+    };
+  }
+
+  handleClick = () => {
+    if (this.state.like) {
+      this.props.minusLike();
+    } else {
+      this.props.plusLike();
+    }
+    this.setState({
+      like: !this.state.like
+    });
+  };
+  render() {
+    return (
+      <div style={{ width: "150px", marginRight: "30px" }}>
+        <img width="150px" src={this.props.event.image} />
+        <h2>{this.props.event.title}</h2>
+        <p>{this.props.event.description}</p>
+        {this.state.like ? (
+          <img onClick={this.handleClick} width="20" src="like.png" />
+        ) : (
+          <img onClick={this.handleClick} width="20" src="unlike.png" />
+        )}
+      </div>
+    );
+  }
+}
+
+function LikeCount(props) {
+  return <p>Количество лайков: {props.count}</p>;
+}
+
+function Events(props) {
   return (
-    <div style={{ display: "flex" }}>
-      {props.events.map(event => {
-        return (
-          <Event
-            key={event.id}
-            item={event}
-            addFavourite={props.addFavourite}
-          />
-        );
-      })}
+    <div>
+      <p>Список событий</p>
+      <div style={{ display: "flex" }}>
+        {props.events.map((event, index) => {
+          return (
+            <Event
+              key={event.id}
+              event={event}
+              plusLike={props.plusLike}
+              minusLike={props.minusLike}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
-function FavouriteList(props) {
-  console.log(props);
-  if (props.list.length == 0) {
-    return <p>Нет избранных событий</p>;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      likeCount: 0
+    };
   }
 
-  return <div>{props.list.map(item => <p key={item.id}>{item.title}</p>)}</div>;
-}
-
-class App extends React.Component {
-  state = {
-    favList: []
+  plusLike = () => {
+    this.setState({
+      likeCount: this.state.likeCount + 1
+    });
   };
 
-  addFavourite = event => {
-    this.state.favList.push(event);
+  minusLike = () => {
     this.setState({
-      favList: [...this.state.favList]
+      likeCount: this.state.likeCount - 1
     });
   };
 
   render() {
     return (
       <div>
-        <EventsList events={events} addFavourite={this.addFavourite} />
-        <FavouriteList list={this.state.favList} />
+        <Events
+          events={events}
+          plusLike={this.plusLike}
+          minusLike={this.minusLike}
+        />
+        <LikeCount count={this.state.likeCount} />
       </div>
     );
   }
